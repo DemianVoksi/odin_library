7066639d6b3cc6d91c1d375932643775478b8814
Demian Voksi <demian.voksi@gmail.com>
2021-12-26 13:28:42 +0100

    initial commit

---
+++ library.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(name, author, length, read) {
+  this.name = name
+  this.author = author
+  this.length = length
+  this.read = read
+}
+
+function addBook() {
+  
+}
