14a2311d100d68adf795f13b2d14e9b72c545697
Demian Voksi <demian.voksi@gmail.com>
2022-01-15 17:40:35 +0100

    add to library

--- library.js
+++ library.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+let myLibrary = [
+  
+];
 
 function Book(name, author, length, read) {
   this.name = name
@@ -7,6 +9,41 @@ function Book(name, author, length, read) {
   this.read = read
 }
 
-function addBook() {
-  
-}
+
+
+
+
+// function addBook() {
+//   let gotm = new Book('Gardens of the Moon', 'Steven Erikson', 712, true);
+//   myLibrary.push(gotm);
+// }
+
+// addBook()
+
+// myLibrary.push(new Book('Deadhouse Gates', 'Steven Erikson', 900, true));
+
+// console.log(myLibrary);
+
+let form = document.querySelector("#form");
+let book = document.querySelector("#book");
+let author = document.querySelector("#author");
+let length = document.querySelector("#length");
+let read = document.querySelector("#read");
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  let bookValue = book.value;
+  let authorValue = author.value;
+  let lengthValue = length.value;
+  let readValue = read.value;
+
+  console.log(bookValue);
+  console.log(authorValue);
+  console.log(lengthValue);
+  console.log(readValue);
+
+  myLibrary.push(new Book(bookValue, authorValue, lengthValue, readValue));
+  console.log(myLibrary);
+});
