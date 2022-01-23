bf137d3c4e3be18036ce6b154c43582c5c3ee79d
Demian Voksi <demian.voksi@gmail.com>
2022-01-24 00:53:16 +0100

    added readme, finished up comments

--- library.js
+++ library.js
@@ -1,5 +1,4 @@
-/* ------ PUTTING A BOOK INTO A LIBRARY ------ */
-
+/* ------ PUTTING A BOOK INTO THE LIBRARY ------ */
 
 // Library variable
 let myLibrary = [
@@ -16,7 +15,6 @@ let myLibrary = [
     read: "yes"
   }
 ];
-
 
 // Book object
 function Book(name, author, length, read) {
@@ -35,25 +33,25 @@ let read = document.querySelector("#read");
 let listHolder = document.querySelector('.list-holder'); // class selector doesn't work but id an query do
 
 
-function setReadValue() {
-  /* Changes the read value from string to boolean */
+// function setReadValue() {
+//   /* Changes the read value from string to boolean */
 
-  if (readValue == "yes") {
-    readValue = true;
-  } else {
-    readValue = false;
-  }
-}
+//   if (readValue == "yes") {
+//     readValue = true;
+//   } else {
+//     readValue = false;
+//   }
+// }
 
-function toggleReadValue() {
-  /* Toggles the read value */
+// function toggleReadValue() {
+//   /* Toggles the read value */
 
-  if (readValue == true) {
-    readValue = false;
-  } else {
-    readValue = true;
-  }
-}
+//   if (readValue == true) {
+//     readValue = false;
+//   } else {
+//     readValue = true;
+//   }
+// }
 
 form.addEventListener('submit', function(e) {
   /* 
@@ -69,17 +67,16 @@ form.addEventListener('submit', function(e) {
   let lengthValue = length.value;
   let readValue = read.value;
 
-  //setReadValue()
+  // setReadValue()
 
   myLibrary.push(new Book(bookValue, authorValue, lengthValue, readValue));
-  /* Only for practice, delete in final version. */
-  console.log(myLibrary);
-
   renderEntry(myLibrary.at(-1), myLibrary.indexOf(myLibrary.at(-1)));
 });
 
-
 /* ------------ */ 
+
+
+/* ------ PULLING THE BOOKS FROM THE LIBRARY ------ */
 
 function removeAllChildren() {
   /* Removes all child nodes from the main list container. 
@@ -92,105 +89,6 @@ function removeAllChildren() {
 };
 
 
-// function createDeleteButton(index) {
-//   /* Creates the delete button. Used by renderEntry() */
-
-//   let deleteSmaller = document.createElement('button');
-//   let buttons = document.querySelector('.buttons');
-//   deleteSmaller.setAttribute('class', 'delete-smaller');
-//   buttons.appendChild(deleteSmaller);
-
-//   let del = document.querySelector('.delete-smaller');
-//   del.innerHTML = 'Delete book';
-
-//   deleteSmaller.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     myLibrary.splice(index, 1);
-//     removeAllChildren();
-//     renderLibrary();
-// });
-// }
-
-function renderEntry(boo, index) {
-  /* Renders an new list item.
-  Used by renderLibrary() */ 
-
-  let listSmaller = document.createElement('div');
-  listSmaller.setAttribute('class', 'list-smaller');
-  listHolder.appendChild(listSmaller);
-
-  let bookData = document.createElement('div');
-  bookData.setAttribute('class', 'book-data');
-  listSmaller.appendChild(bookData);
-
-  let nameSmaller = document.createElement('div');
-  nameSmaller.setAttribute('class', 'name-smaller');
-  bookData.appendChild(nameSmaller);
-  let nameText = document.createTextNode(boo.name);
-  nameSmaller.appendChild(nameText);
-
-  let authorSmaller = document.createElement('div');
-  authorSmaller.setAttribute('class', 'author-smaller');
-  bookData.appendChild(authorSmaller);
-  let authorText = document.createTextNode(boo.author);
-  authorSmaller.appendChild(authorText);
-
-  let lengthSmaller = document.createElement('div');
-  lengthSmaller.setAttribute('class', 'length-smaller');
-  bookData.appendChild(lengthSmaller);
-  let lengthText = document.createTextNode(boo.length);
-  lengthSmaller.appendChild(lengthText);
-
-  let buttons = document.createElement('div');
-  buttons.setAttribute('class', 'buttons');
-  listSmaller.appendChild(buttons);
-
-  let readSmaller = document.createElement('button');
-  readSmaller.setAttribute('class', 'read-smaller');
-  buttons.appendChild(readSmaller);
-  readSmaller.innerHTML = setReadText(boo.read);
-  // let btn = document.querySelector('.read-smaller');
-  // btn.innerHTML = setReadText(boo.read);
-
-  readSmaller.addEventListener('click', function(e) {
-    e.preventDefault()
-    //let btn = document.querySelector('.read-smaller');
-
-    if (readSmaller.innerHTML == "Read") {
-    readSmaller.innerHTML = "Unread";
-  } else {
-    readSmaller.innerHTML = "Read";
-  }
-  });
-
-//  createDeleteButton(index);
-
-  let deleteSmaller = document.createElement('button');
-  deleteSmaller.setAttribute('class', 'delete-smaller');
-  buttons.appendChild(deleteSmaller);
-  deleteSmaller.innerHTML = 'Delete book';
-
-  deleteSmaller.addEventListener('click', (e) => {
-    e.preventDefault()
-    myLibrary.splice(index, 1);
-    removeAllChildren();
-    renderLibrary();
-});
-}
-
-function renderLibrary() {
-  /* Renders the whole library stored in myLibrary.
-  Uses renderEntry() */
-
-  removeAllChildren();
-
-  myLibrary.forEach((book, index) => {
-    renderEntry(book, index)
-  });
-}
-
-window.onload = renderLibrary();
-
 function setReadText(readValue) {
   /* Changes the read value from string to boolean */
   let value;
@@ -202,3 +100,93 @@ function setReadText(readValue) {
   }
   return value;
 }
+
+
+function renderEntry(boo, index) {
+  /* Renders an new list item. Used by renderLibrary()
+  and the form event listener. */ 
+
+  // container for the list
+  let listSmaller = document.createElement('div');
+  listSmaller.setAttribute('class', 'list-smaller');
+  listHolder.appendChild(listSmaller);
+
+  // container for the name, author and length
+  let bookData = document.createElement('div');
+  bookData.setAttribute('class', 'book-data');
+  listSmaller.appendChild(bookData);
+
+  // name
+  let nameSmaller = document.createElement('div');
+  nameSmaller.setAttribute('class', 'name-smaller');
+  bookData.appendChild(nameSmaller);
+  let nameText = document.createTextNode(boo.name);
+  nameSmaller.appendChild(nameText);
+
+  // author
+  let authorSmaller = document.createElement('div');
+  authorSmaller.setAttribute('class', 'author-smaller');
+  bookData.appendChild(authorSmaller);
+  let authorText = document.createTextNode(boo.author);
+  authorSmaller.appendChild(authorText);
+
+  // length
+  let lengthSmaller = document.createElement('div');
+  lengthSmaller.setAttribute('class', 'length-smaller');
+  bookData.appendChild(lengthSmaller);
+  let lengthText = document.createTextNode(boo.length);
+  lengthSmaller.appendChild(lengthText);
+
+  // container for buttons
+  let buttons = document.createElement('div');
+  buttons.setAttribute('class', 'buttons');
+  listSmaller.appendChild(buttons);
+
+  // read button
+  let readSmaller = document.createElement('button');
+  readSmaller.setAttribute('class', 'read-smaller');
+  buttons.appendChild(readSmaller);
+  readSmaller.innerHTML = setReadText(boo.read);
+
+  // read/unread event listener
+  readSmaller.addEventListener('click', function(e) {
+    e.preventDefault()
+
+    if (readSmaller.innerHTML == "Read") {
+    readSmaller.innerHTML = "Unread";
+  } else {
+    readSmaller.innerHTML = "Read";
+  }
+  });
+
+  // delete button
+  let deleteSmaller = document.createElement('button');
+  deleteSmaller.setAttribute('class', 'delete-smaller');
+  buttons.appendChild(deleteSmaller);
+  deleteSmaller.innerHTML = 'Delete book';
+
+  // delete event listener
+  deleteSmaller.addEventListener('click', (e) => {
+    e.preventDefault()
+    myLibrary.splice(index, 1);
+    removeAllChildren();
+    renderLibrary();
+});
+}
+
+
+function renderLibrary() {
+  /* Renders the whole library stored in myLibrary.
+  Uses renderEntry() and removeAllChildren() */
+
+  removeAllChildren();
+  myLibrary.forEach((book, index) => {
+    renderEntry(book, index)
+  });
+}
+
+/* ------------ */
+
+
+// starts the process when window opens
+window.onload = renderLibrary();
